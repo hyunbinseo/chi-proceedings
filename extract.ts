9ffdff27e8340495c24c686dbc2e1ee71907e454
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-03-21 10:40:02 +0900

    chore: extract topics with keywords

---
+++ extract.ts
@@ -0,0 +1,18 @@
+import { globSync, readFileSync, writeFileSync } from 'node:fs';
+import { basename, join } from 'node:path';
+
+const topicAndKeywords = new Map([
+	['elderly', ['elderly', 'older']], //
+]);
+
+for (const [topic, keywords] of topicAndKeywords) {
+	const regex = new RegExp(`^.*?(${keywords.map((k) => `\\b${k}\\b`).join('|')}).*?$`, 'gim');
+	const paths = globSync('./table-of-contents/*.txt');
+	const matches = paths.flatMap((path) => {
+		const text = readFileSync(path, { encoding: 'utf8' });
+		return [...text.matchAll(regex)].map((match) =>
+			`${match[0]} ('${basename(path, '.txt')})`.replaceAll(/ {2,}/g, ' '),
+		);
+	});
+	writeFileSync(join(import.meta.dirname, `/extracted/${topic}.txt`), matches.join('\n'));
+}
